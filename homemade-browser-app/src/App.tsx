099d08490f740a5e066da410331766f96ae77ba8
Vico <Vico>
2022-10-12 15:20:47 +0200

    Apollo client + skeleton of search engine done

--- homemade-browser-app/src/App.tsx
+++ homemade-browser-app/src/App.tsx
@@ -1,24 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useContext } from "react";
 import "./App.css";
+import SearchBar from "./components/SearchBar";
+import { ResearchContext } from "./context/ResearchContext";
+import Result from "./components/Result";
 
-const App: () => JSX.Element = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App: () => JSX.Element = () => {
+  const { researchResult } = useContext(ResearchContext);
+  return (
+    <div>
+      <SearchBar />
+      {researchResult.map((item, index) => (
+        <Result key={index} {...item} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
